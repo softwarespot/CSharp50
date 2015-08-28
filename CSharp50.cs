95d24401b4607b9421a706e17a8cc12ef2c314be
softwarespot <softwarespotapps@gmail.com>
2015-08-28 21:02:27 +0300

    Initial commit

---
+++ CSharp50.cs
@@ -0,0 +1,266 @@
+﻿using System;
+
+namespace CSharp50
+{
+    /// <summary>
+    ///     Based on the original CS50 Library 5, but ported across to C#
+    ///     https://manual.cs50.net/library
+    ///     All rights belong to the authors of the original CS50 library
+    /// </summary>
+    public static class CSharp50
+    {
+        /// <summary>
+        ///     Reads a line of text from standard input and returns the equivalent
+        ///     char; if text does not represent a char, user is prompted to retry.
+        ///     Leading and trailing whitespace is ignored.  If line can't be read,
+        ///     returns Char.MaxValue.
+        /// </summary>
+        /// <returns>User input char; otherwise, Char.MaxValue.</returns>
+        public static char GetChar()
+        {
+            // Try to get a char from user
+            while (true)
+            {
+                string line;
+                try
+                {
+                    // Get line of text, returning Char.MaxValue on failure
+                    line = Console.ReadLine();
+                    if (line == null)
+                    {
+                        return Char.MaxValue;
+                    }
+                }
+                catch
+                {
+                    return Char.MaxValue;
+                }
+
+                // Trim leading and/or trailing whitespace
+                line = line.Trim();
+
+                // Return a char if only a char (possibly with
+                // leading and/or trailing whitespace) was provided
+                if (line.Length == 1)
+                {
+                    return line[0];
+                }
+
+                Console.Write("Retry: ");
+            }
+        }
+
+        /// <summary>
+        ///     Reads a line of text from standard input and returns the equivalent
+        ///     double as precisely as possible; if text does not represent a
+        ///     double, user is prompted to retry.  Leading and trailing whitespace
+        ///     is ignored.  For simplicity, overflow and underflow are not detected.
+        ///     If line can't be read, returns Double.MaxValue.
+        /// </summary>
+        /// <returns>User input double; otherwise, Double.MaxValue.</returns>
+        public static double GetDouble()
+        {
+            // Try to get a double from user
+            while (true)
+            {
+                string line;
+                try
+                {
+                    // Get line of text, returning Double.MaxValue on failure
+                    line = Console.ReadLine();
+                    if (line == null)
+                    {
+                        return Double.MaxValue;
+                    }
+                }
+                catch
+                {
+                    return Double.MaxValue;
+                }
+
+                // Trim leading and/or trailing whitespace
+                line = line.Trim();
+
+                // Return a double if only a double (possibly with
+                // leading and/or trailing whitespace) was provided
+                double value;
+                if (Double.TryParse(line, out value))
+                {
+                    return value;
+                }
+
+                Console.Write("Retry: ");
+            }
+        }
+
+        /// <summary>
+        ///     Reads a line of text from standard input and returns the equivalent
+        ///     float as precisely as possible; if text does not represent a float,
+        ///     user is prompted to retry.  Leading and trailing whitespace is ignored.
+        ///     For simplicity, overflow and underflow are not detected.  If line can't
+        ///     be read, returns Single.MaxValue.
+        /// </summary>
+        /// <returns>User input float; otherwise, Single.MaxValue.</returns>
+        public static float GetFloat()
+        {
+            // Try to get a float from user
+            while (true)
+            {
+                string line;
+                try
+                {
+                    // Get line of text, returning Single.MaxValue on failure
+                    line = Console.ReadLine();
+                    if (line == null)
+                    {
+                        return Single.MaxValue;
+                    }
+                }
+                catch
+                {
+                    return Single.MaxValue;
+                }
+
+                // Trim leading and/or trailing whitespace
+                line = line.Trim();
+
+                // Return a float if only a float (possibly with
+                // leading and/or trailing whitespace) was provided
+                float value;
+                if (Single.TryParse(line, out value))
+                {
+                    return value;
+                }
+
+                Console.Write("Retry: ");
+            }
+        }
+
+        /// <summary>
+        ///     Reads a line of text from standard input and returns it as an
+        ///     int in the range of [-2^31 + 1, 2^31 - 2], if possible; if text
+        ///     does not represent such an int, user is prompted to retry.  Leading
+        ///     and trailing whitespace is ignored.  For simplicity, overflow is not
+        ///     detected.  If line can't be read, returns Int32.MaxValue.
+        /// </summary>
+        /// <returns>User input int; otherwise, Int32.MaxValue.</returns>
+        public static int GetInt()
+        {
+            // Try to get an int from user
+            while (true)
+            {
+                string line;
+                try
+                {
+                    // Get line of text, returning Int32.MaxValue on failure
+                    line = Console.ReadLine();
+                    if (line == null)
+                    {
+                        return Int32.MaxValue;
+                    }
+                }
+                catch
+                {
+                    return Int32.MaxValue;
+                }
+
+                // Trim leading and/or trailing whitespace
+                line = line.Trim();
+
+                // Return an int if only an int (possibly with
+                // leading and/or trailing whitespace) was provided
+                int value;
+                if (Int32.TryParse(line, out value))
+                {
+                    return value;
+                }
+
+                Console.Write("Retry: ");
+            }
+        }
+
+        /// <summary>
+        ///     Reads a line of text from standard input and returns the equivalent
+        ///     long in the range [-2^63 + 1, 2^63 - 2], if possible; if text
+        ///     does not represent such a long, user is prompted to retry.
+        ///     Leading and trailing whitespace is ignored.  For simplicity, overflow
+        ///     is not detected.  If line can't be read, returns Int64.MaxValue.
+        /// </summary>
+        /// <returns>User input long; otherwise, Int64.MaxValue.</returns>
+        public static long GetLong()
+        {
+            // Try to get a long from user
+            while (true)
+            {
+                string line;
+                try
+                {
+                    // Get line of text, returning Int64.MaxValue on failure
+                    line = Console.ReadLine();
+                    if (line == null)
+                    {
+                        return Int64.MaxValue;
+                    }
+                }
+                catch
+                {
+                    return Int64.MaxValue;
+                }
+
+                // Trim leading and/or trailing whitespace
+                line = line.Trim();
+
+                // Return a double if only a double (possibly with
+                // leading and/or trailing whitespace) was provided
+                long value;
+                if (Int64.TryParse(line, out value))
+                {
+                    return value;
+                }
+
+                Console.Write("Retry: ");
+            }
+        }
+
+        // GetString
+        /// <summary>
+        ///     Reads a line of text from standard input and returns it as a
+        ///     string (char*), sans trailing newline character.  (Ergo, if
+        ///     user inputs only "\n", returns "" not NULL.)  Returns NULL
+        ///     upon error or no input whatsoever (i.e., just EOF).  Leading
+        ///     and trailing whitespace is not ignored.
+        /// </summary>
+        /// <returns>User input string; otherwise, null.</returns>
+        public static string GetString()
+        {
+            while (true)
+            {
+                string line;
+                try
+                {
+                    // Get line of text, returning null on failure
+                    line = Console.ReadLine();
+                    if (line == null)
+                    {
+                        return null;
+                    }
+                }
+                catch
+                {
+                    return null;
+                }
+
+                // Check if line is am EOL character
+                if (line == Environment.NewLine)
+                {
+                    return String.Empty;
+                }
+
+                // Trim leading and/or trailing whitespace
+                line = line.Trim();
+
+                return line;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # CSharp50
-A port of CS50 to C#
+
+The following library is a port of the CS50 library. Details about the original library and what it's all about, can be found at https://manual.cs50.net/library.
+
+## How to use
+
+Download the repository, extract the contents and open Visual Studio. Once complete, right-click on the solution in the `solution explorer` pane and choose `add item`. Navigate to the directory where the contents of the repository when extracted and select `CSharp.cs`. This will copy the file to your project and add a reference to the file.
+
+## Example
+
+```csharp
+    string userChoice = CSharp.GetString();
+    Console.WriteLine("String entered: " + userChoice);
+```
